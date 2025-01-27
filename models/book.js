5b1e87267dcc516e575c0e7abe0974978f143af4
vkavith <vkavith@gmail.com>
2025-01-27 15:00:22 -0500

    Capstone Library Backend

---
+++ models/book.js
@@ -0,0 +1,34 @@
+// models/Book.js
+const mongoose = require("mongoose");
+
+const BookSchema = new mongoose.Schema({
+  bookName: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  isbn: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  stock: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+
+  imageUrl: {
+    type: String,
+  },
+});
+
+module.exports = mongoose.model("Book", BookSchema);
---
+++ models/bookTransaction.js
@@ -0,0 +1,32 @@
+// models/bookTransaction.js
+const mongoose = require("mongoose");
+
+const TransactionSchema = new mongoose.Schema({
+  book: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Book",
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  issueDate: {
+    type: Date,
+    default: Date.now,
+  },
+  dueDate: {
+    type: Date,
+    required: true,
+  },
+  returnDate: {
+    type: Date,
+  },
+  status: {
+    type: String,
+    enum: ["issued", "returned"],
+  },
+});
+
+module.exports = mongoose.model("Transaction", TransactionSchema);
