00a44ca39162bda8beb81dbc14410efd69e7073b
vkavith <vkavith@gmail.com>
2025-01-28 15:33:35 -0500

    Capstone Library Backend

--- server.js
+++ server.js
@@ -1,1 +1,43 @@
-console.log('Hello')
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+const conn = require("./config/db");
+conn();
+
+const starterBooks = require("./config/bookSeed");
+const Book = require("./models/book");
+
+const bookRoutes = require("./routes/bookRoutes");
+
+app.use(express.json());
+app.use("/api/books", bookRoutes);
+
+//home route
+app.get("/", (req,res) => {
+    res.send(" Library Management System");
+});
+
+//Seed the initial data for Books
+
+app.get("/api/seed/books", async(req, res) => {
+    try {
+        //Clear existing data
+        await Book.deleteMany({});
+   
+        //Insert new Book Data
+        const seedBooks = await Book.create(starterBooks);
+        console.log("Database seeded with Books Data successfully");
+        res.json(seedBooks);
+
+    } catch(error) {
+        console.error("Seeding error", error.message);
+        res.status(500).json({error: error.message});
+    }
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`listening on port ${process.env.PORT}`);
+});
+    
+    )
