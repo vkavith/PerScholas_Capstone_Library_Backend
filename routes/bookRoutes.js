ce3c6e089f10c9f104a19ce1d2efce7969be54d3
vkavith <vkavith@gmail.com>
2025-01-28 15:16:27 -0500

    Capstone Library Backend

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -4,51 +4,60 @@ const Book = require("../models/book");
 
 //get all books
 
-router.get("/", async(req, res) => {
-    try{
-        const allBooks = await Book.find({});
-        res.json(allBooks);
-    }catch(error) {
-        res.status(500).json({error: error.message});
-    }
+router.get("/", async (req, res) => {
+  try {
+    const allBooks = await Book.find({});
+    res.json(allBooks);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 //create a new Book
 
-router.post("/", async(req,res) => {
+router.post("/", async (req, res) => {
+  console.log(req.body);
+
+  try {
+    const createBook = await Book.create(req.body);
     console.log(req.body);
-
-    try{
-        const createBook = await Book.create(req.body);
-        console.log(req.body);
-        res.json(createBook);
-    } catch(error) {
-        res.status(500).json({error: error.message});
-    }
+    res.json(createBook);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
-
 
 // Get 1 book based on ISBN or bookname
 
-router.get("/:id", async(req,res) => {
-    try{
-        const singleBook = await Book.findById(req.params.id);
-        res.json(singleBook);
-    }catch(error) {
-        res.status(500).json({error: error.message});
-    }
+router.get("/:id", async (req, res) => {
+  try {
+    const singleBook = await Book.findById(req.params.id);
+    res.json(singleBook);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 // Update Book Details including the Stock
 
-router.put("/:id", async(req,res) => {
-    try{
-        const updateBook = await Book.findByIdAndUpdate(req.params.id, req.body);
-        res.json(updateBook);
-    }catch(error) {
-        res.status(500).json({error: error.message});
-    }
+router.put("/:id", async (req, res) => {
+  try {
+    const updateBook = await Book.findByIdAndUpdate(req.params.id, req.body);
+    res.json(updateBook);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
-//Delete a Book 
+//Delete a Book
 
+router.delete("/:id", async (req, res) => {
+  try {
+    const deleteBook = await Book.findByIdAndDelete(req.params.id);
+    res.json(deleteBook);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+module.exports = router;
