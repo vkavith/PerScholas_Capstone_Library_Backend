31c813604155d09ec67849fc07d68cfd9f2ac104
vkavith <vkavith@gmail.com>
2025-01-30 15:53:38 -0500

    Capstone Backend

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const express = require("express");
+//const mongoose = require("mongoose");
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 const conn = require("./config/db");
 conn();
@@ -10,34 +12,38 @@ const Book = require("./models/book");
 
 const bookRoutes = require("./routes/bookRoutes");
 
+const option = {
+  origin: "https://perscholas-capstone-library-frontend.onrender.com",
+  methods: "GET,PUT,PATCH,POST,DELETE",
+  credentials: true,
+};
+
+app.use(cors(option));
 app.use(express.json());
 app.use("/api/books", bookRoutes);
 
 //home route
-app.get("/", (req,res) => {
-    res.send(" Library Management System");
+app.get("/", (req, res) => {
+  res.send(" Library Management System");
 });
 
 //Seed the initial data for Books
 
-app.get("/api/seed/books", async(req, res) => {
-    try {
-        //Clear existing data
-        await Book.deleteMany({});
-   
-        //Insert new Book Data
-        const seedBooks = await Book.create(starterBooks);
-        console.log("Database seeded with Books Data successfully");
-        res.json(seedBooks);
+app.get("/api/seed/books", async (req, res) => {
+  try {
+    //Clear existing data
+    await Book.deleteMany({});
 
-    } catch(error) {
-        console.error("Seeding error", error.message);
-        res.status(500).json({error: error.message});
-    }
+    //Insert new Book Data
+    const seedBooks = await Book.create(starterBooks);
+    console.log("Database seeded with Books Data successfully");
+    res.json(seedBooks);
+  } catch (error) {
+    console.error("Seeding error", error.message);
+    res.status(500).json({ error: error.message });
+  }
 });
 
 app.listen(process.env.PORT, () => {
-    console.log(`listening on port ${process.env.PORT}`);
+  console.log(`listening on port ${process.env.PORT}`);
 });
-    
-    
